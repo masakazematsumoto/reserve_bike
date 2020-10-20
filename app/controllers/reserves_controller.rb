5fd782cafa0f18bf3f6721a9c81ddf26708dacf7
masakazematsumoto <kazecocoa0919@gmail.com>
2020-10-20 16:24:32 +0900

    パラメーター

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -8,4 +8,8 @@ def index
   # end
 end
 
+def reserve_palams
+  palams.require(:reserve).permit(:reserve1, :reserve2, :reserve3, :reserve4, :reserve5, :reserve6, :reserve7, :reserve8, :reserve9,)
+end
+
 end
